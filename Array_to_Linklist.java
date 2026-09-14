48160968dd7a4fe9a0acb67fc1b34c4067d0b4fc
Aditya Singh <adityasinghrajput1474@gmail.com>
2026-09-14 22:38:47 +0530

    Implement array to linked list conversion

---
+++ Array_to_Linklist.java
@@ -0,0 +1,26 @@
+/*
+// Representation of a node
+class Node {
+    int data;
+    Node next;
+    Node (int d) {
+       data = d;
+       next = null;
+    }
+};
+*/
+class Solution {
+    public Node arrayToList(int arr[]) {
+        // code here
+        if(arr.length==0) return null;
+        Node head=new Node(arr[0]);
+        Node temp=head;
+        
+        for(int i=1;i<arr.length;i++){
+            Node newNode=new Node(arr[i]);
+            temp.next=newNode;
+            temp=newNode;
+        }
+        return head;
+    }
+}
